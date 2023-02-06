209af01c0f0759c1d32ea876e2aa15c4140b7ffd
vgojayev <vahid.qocayev.2014@gmail.com>
2023-02-06 09:30:52 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           >
           </a>
           <h3>
-            CloudAcademy ❤ DevOps 2019 - version 2.0 - Demo01
+            CloudAcademy ❤ DevOps 2019 - version 2.0 - Demo02
           </h3>
         </header>
       </div>
